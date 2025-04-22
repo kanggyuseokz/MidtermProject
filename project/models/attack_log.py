5da209b666150c2e4e6c0c6f2c6acd7702e5bd64
[kanggyuseokz] <[gnlvkfka1592@naver.com]>
2025-04-22 18:22:51 +0900

    cleanup2

---
+++ project/models/attack_log.py
@@ -0,0 +1,10 @@
+from project import db
+
+class AttackLog(db.Model):
+    id = db.Column(db.Integer, primary_key=True)  # 고유 ID
+    timestamp = db.Column(db.String(100))         # 공격 시각
+    username = db.Column(db.String(100))          # 사용자 이름
+    ip = db.Column(db.String(100))                # IP 주소
+    attack_type = db.Column(db.String(20))        # 공격 유형 (XSS, SQLi 등)
+    original = db.Column(db.Text)                 # 원본 입력
+    cleaned = db.Column(db.Text)                  # 정제된 입력
